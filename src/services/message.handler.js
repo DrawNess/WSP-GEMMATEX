27cec66b20c59b34ca626d22e68019c034b3b9f9
DrawNess <partunax4830@proton.me>
2026-05-02 11:53:56 -0400

    Respuesta a las opciones de menu de bienvenida.

--- src/services/message.handler.js
+++ src/services/message.handler.js
@@ -16,6 +16,10 @@ class MessageHandler {
       await whatsappService.sendMessage(message.from, response, message.id);
     }
     await whatsappService.markAsRead(message.id);
+    if(message?.type === 'interactive') {
+      const option = message.interactive?.button_reply?.id.toLowerCase().trim();
+      await this.handleMenuOption(message.from, option);
+    }
   }
 
   isGreeting(message) {
@@ -39,8 +43,30 @@ class MessageHandler {
       { type: 'reply', reply: { id: 'option_1', title: 'Ver productos' } },
       { type: 'reply', reply: { id: 'option_2', title: 'Contactar soporte' } },
       { type: 'reply', reply: { id: 'option_3', title: 'Horarios de atención' } },
+      { type: 'reply', reply: { id: 'option_4', title: 'Ver Ubicacion' } },
     ];
     await whatsappService.sendInteractiveButtons(to, menuMessage, buttons);
+  }
+
+  async handleMenuOption(to, option) {
+    let response;
+    switch (option) {
+      case 'option_1':
+        response = 'Aquí están nuestros productos...';
+        break;
+      case 'option_2':
+        response = 'Puedes contactar a nuestro soporte en aqui'
+        break;
+      case 'option_3':
+        response = 'Nuestros horarios de atención son...';
+        break;
+      case 'option_4':
+        response = 'Nuestra ubicación es...';
+        break;
+      default:
+        response = 'Lo siento, no entendí esa opción. Por favor, elige una opción válida.';
+    }
+    await whatsappService.sendMessage(message.from, response);
   }
 }
 
